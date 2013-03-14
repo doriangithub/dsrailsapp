041e16b2dd510f982b9472808db4ffad1c1554c9
Dorian Sharevich <dorian.sharevich@gmail.com>
2013-03-13 19:19:39 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dsrailsapp::Application.config.secret_token = 'd57cd45ae1e863e055776b530e64cd6fef0ea2290694dfffaa82f94fcb31690ce70ea36c4eb21f1d256375622ebc007bc128a19368ecc4eff639ce1792570606'
